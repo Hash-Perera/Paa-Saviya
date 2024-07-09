cb00f183e62dd9c5b39e4002f69318974fa67b96
Hashan Perera <hashperera.v@gmail.com>
2024-07-09 21:48:53 +0530

    home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,14 @@
 import "./App.css";
 import { Grid, Typography } from "@mui/material";
+import AppHeader from "./components/header";
+import Home from "./pages/home";
 
 function App() {
-  return <>This is new project</>;
+  return (
+    <>
+      <Home />
+    </>
+  );
 }
 
 export default App;
---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,11 @@
+import AppHeader from "../components/header";
+
+const Home = () => {
+  return (
+    <div>
+      <AppHeader />
+    </div>
+  );
+};
+
+export default Home;
