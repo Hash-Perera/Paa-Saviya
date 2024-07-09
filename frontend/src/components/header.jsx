244c695f6fbb131008740f6a1003d0d09476f8c7
Hashan Perera <hashperera.v@gmail.com>
2024-07-10 00:54:06 +0530

    Home page

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -12,6 +12,7 @@ import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
 import AdbIcon from "@mui/icons-material/Adb";
+import Logo from "../assets/images/logo-small.png";
 
 const pages = ["Home", "Projects", "About", "Contact"];
 // const settings = ["Profile", "Account", "Dashboard", "Logout"];
@@ -36,28 +37,19 @@ function AppHeader() {
   };
 
   return (
-    <AppBar position="static">
+    <AppBar
+      position="fixed"
+      sx={{ backgroundColor: "white", boxShadow: "none" }}
+    >
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          {/* <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: "none", md: "flex" },
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            LOGO
-          </Typography> */}
+          <img
+            src={Logo}
+            alt="organization logo"
+            style={{ height: "5.0rem" }}
+          />
 
+          {/* Small Screen */}
           <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
             <IconButton
               size="large"
@@ -65,7 +57,7 @@ function AppHeader() {
               aria-controls="menu-appbar"
               aria-haspopup="true"
               onClick={handleOpenNavMenu}
-              color="inherit"
+              color="black"
             >
               <MenuIcon />
             </IconButton>
@@ -95,67 +87,23 @@ function AppHeader() {
             </Menu>
           </Box>
 
-          {/* Small Screen */}
-          {/* <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
+          {/* Large Screen */}
+          <Box
             sx={{
-              mr: 2,
-              display: { xs: "flex", md: "none" },
               flexGrow: 1,
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
+              display: { xs: "none", md: "flex" },
             }}
           >
-            LOGO
-          </Typography> */}
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             {pages.map((page) => (
               <Button
                 key={page}
                 onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: "white", display: "block" }}
+                sx={{ my: 2, color: "black", display: "block" }}
               >
                 {page}
               </Button>
             ))}
           </Box>
-
-          {/* <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: "45px" }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box> */}
         </Toolbar>
       </Container>
     </AppBar>
--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,10 +1,161 @@
+import Footer from "../components/footer";
 import AppHeader from "../components/header";
+import { Grid, Typography } from "@mui/material";
+import CommonCard from "../components/shared/common-card";
+import backgroundImage from "../assets/images/sample-hero.jpg";
 
 const Home = () => {
+  const images = [1, 2, 3];
+  const aboutUs = ["Our Vision", "Our Founders", "Our Services"];
   return (
-    <div>
+    <>
       <AppHeader />
-    </div>
+      <div
+        className="d-flex align-items-center justify-content-center vh-100"
+        style={{
+          backgroundImage: `url(${backgroundImage})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        <h1 style={{ color: "#000" }}>Home</h1>
+      </div>
+
+      {/* Projects section */}
+      <div
+        style={{
+          paddingLeft: "12%",
+          paddingRight: "12%",
+          paddingTop: "5rem",
+          paddingBottom: "5rem",
+        }}
+        className="bg-light"
+      >
+        <Grid container spacing={1}>
+          <Grid
+            item
+            xs={12}
+            sm={12}
+            md={12}
+            className="d-flex flex-column align-items-center mb-5"
+          >
+            <Typography variant="h4" color="textPrimary">
+              Our Projects
+            </Typography>
+            <Typography variant="body1" color="textSecondary">
+              77% of learners report career benefits, like landing a new job,
+              earning a promotion, gaining applicable skills, and more.
+            </Typography>
+          </Grid>
+
+          {images.map((image, index) => (
+            <Grid
+              key={index}
+              item
+              xs={12}
+              sm={6}
+              md={4}
+              className="d-flex align-items-center justify-content-center"
+            >
+              <CommonCard
+                style={{ position: "relative", width: "100%" }}
+                className="d-flex flex-column align-items-center justify-content-center"
+              >
+                <img
+                  style={{ maxHeight: "100%", maxWidth: "100%" }}
+                  src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/A6A0477DA809F962FBA9A54E53C40E66F6182B822AC8CF0AD1BCDFB8D8CBEC6A/scale?width=1200&aspectRatio=1.78&format=webp"
+                  alt={`Project Image ${image}`}
+                />
+                <Typography
+                  variant="body1"
+                  color="textPrimary"
+                  style={{
+                    position: "absolute",
+                    top: "50%",
+                    left: "50%",
+                    transform: "translate(-50%, -50%)",
+                    backgroundColor: "rgba(0, 0, 0, 0.5)",
+                    color: "white",
+                    padding: "5px 10px",
+                    borderRadius: "5px",
+                  }}
+                >
+                  Project {image}
+                </Typography>
+              </CommonCard>
+            </Grid>
+          ))}
+        </Grid>
+      </div>
+
+      {/* About Us section */}
+      <div
+        style={{
+          paddingLeft: "12%",
+          paddingRight: "12%",
+          paddingTop: "5rem",
+          paddingBottom: "5rem",
+          backgroundColor: "white",
+        }}
+      >
+        <Grid container spacing={1}>
+          <Grid
+            item
+            xs={12}
+            sm={12}
+            md={12}
+            className="d-flex flex-column align-items-center mb-5"
+          >
+            <Typography variant="h4" color="textPrimary">
+              About Us
+            </Typography>
+            <Typography variant="body1" color="textSecondary">
+              77% of learners report career benefits, like landing a new job,
+              earning a promotion, gaining applicable skills, and more.
+            </Typography>
+          </Grid>
+
+          {aboutUs.map((image, index) => (
+            <Grid
+              key={index}
+              item
+              xs={12}
+              sm={6}
+              md={4}
+              className="d-flex align-items-center justify-content-center"
+            >
+              <CommonCard
+                style={{ position: "relative", width: "100%" }}
+                className="d-flex flex-column align-items-center justify-content-center"
+              >
+                <img
+                  style={{ maxHeight: "100%", maxWidth: "100%" }}
+                  src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/A6A0477DA809F962FBA9A54E53C40E66F6182B822AC8CF0AD1BCDFB8D8CBEC6A/scale?width=1200&aspectRatio=1.78&format=webp"
+                  alt={`Project Image ${image}`}
+                />
+                <Typography
+                  variant="body1"
+                  color="textPrimary"
+                  style={{
+                    position: "absolute",
+                    top: "50%",
+                    left: "50%",
+                    transform: "translate(-50%, -50%)",
+                    backgroundColor: "rgba(0, 0, 0, 0.5)",
+                    color: "white",
+                    padding: "5px 10px",
+                    borderRadius: "5px",
+                  }}
+                >
+                  Project {image}
+                </Typography>
+              </CommonCard>
+            </Grid>
+          ))}
+        </Grid>
+      </div>
+      <Footer />
+    </>
   );
 };
 
