fcefd026fa8ecd1bf26b716ceaecad575da3f50a
Achintha <achinthaperis64@gmail.com>
2024-07-11 19:01:02 +0530

    About us completed

--- frontend/src/pages/aboutUs.jsx
+++ frontend/src/pages/aboutUs.jsx
@@ -4,6 +4,7 @@ import { motion } from "framer-motion";
 import { BsEye } from "react-icons/bs";
 import { FaHandsHelping } from "react-icons/fa";
 import { useInView } from "react-intersection-observer";
+import "../styles/aboutUsPage.css";
 
 const AboutUs = () => {
   const { ref: firstSectionRef, inView: firstSectionInView } = useInView({
@@ -17,6 +18,11 @@ const AboutUs = () => {
   });
 
   const { ref: thirdSectionRef, inView: thirdSectionInView } = useInView({
+    triggerOnce: true,
+    threshold: 0.1,
+  });
+
+  const { ref: fourthSectionRef, inView: fourthSectionInView } = useInView({
     triggerOnce: true,
     threshold: 0.1,
   });
@@ -57,126 +63,189 @@ const AboutUs = () => {
 
   return (
     <>
+      {/* What is Paada Saviya section */}
       <div
-        style={{ marginTop: "150px" }}
-        className="container-fluid py-5 bg-gray"
+        style={{ marginTop: "100px" }}
+        className="container-fluid bg-gray pb-5"
       >
+        <div className="row mb-4">
+          <div className="col text-center">
+            <motion.h1
+              ref={firstSectionRef}
+              initial={{ opacity: 0, y: -100 }}
+              animate={firstSectionInView ? { opacity: 1, y: 0 } : {}}
+              transition={{ duration: 1.2 }}
+              style={{ fontSize: "4em" }}
+              className="vision-text font-weight-bold"
+            >
+              What is Paa-Saviya<span className="question-mark ml-3">?</span>
+            </motion.h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-10 mx-auto">
+            <motion.div
+              ref={firstSectionRef}
+              initial={{ opacity: 0, x: -500 }}
+              animate={firstSectionInView ? { opacity: 1, x: 0 } : {}}
+              transition={{ duration: 2 }}
+              className="custom-card-2 card border mb-5"
+            >
+              <div className="card-body">
+                <p
+                  style={{
+                    lineHeight: 2,
+                    letterSpacing: "0.05em",
+                    fontSize: "1.1em",
+                  }}
+                  className="card-title m-0 text-center vision-text font-weight-bold"
+                >
+                  Paa-Saviya is an organization within the University of
+                  Colombo, Faculty of Medicine, established two years ago to
+                  address the significant yet neglected area of wound care in
+                  Sri Lanka. Wound care is an unquantified and poorly studied
+                  field that requires dedicated attention and resources.
+                </p>
+              </div>
+            </motion.div>
+
+            <motion.div
+              ref={firstSectionRef}
+              initial={{ opacity: 0, x: 500 }}
+              animate={firstSectionInView ? { opacity: 1, x: 0 } : {}}
+              transition={{ duration: 2 }}
+              className="custom-card-2 card border mb-5"
+            >
+              <div className="card-body">
+                <p
+                  style={{
+                    lineHeight: 2,
+                    letterSpacing: "0.05em",
+                    fontSize: "1.1em",
+                  }}
+                  className="card-title m-0 text-center vision-text font-weight-bold"
+                >
+                  Paa-Saviya is committed to providing comprehensive wound care
+                  and other clinical services, conducting rigorous research with
+                  the collaboration of other prestigious universities,
+                  developing innovative wound care products, engaging in
+                  educational activities, trainings and fashioning awareness.
+                </p>
+              </div>
+            </motion.div>
+            <motion.div
+              ref={fourthSectionRef}
+              initial={{ opacity: 0, x: -500 }}
+              animate={fourthSectionInView ? { opacity: 1, x: 0 } : {}}
+              transition={{ duration: 2 }}
+              className="custom-card-2 card border mb-3"
+            >
+              <div className="card-body">
+                <p
+                  style={{
+                    lineHeight: 2,
+                    letterSpacing: "0.05em",
+                    fontSize: "1.1em",
+                  }}
+                  className="card-title m-0 text-center vision-text font-weight-bold"
+                >
+                  Our team comprises professionals from diverse sectors,
+                  including healthcare, sciences, technology, and social
+                  services, all united by a common goal: to offer the best
+                  possible care for patients with complex diseases and
+                  conditions. As a proud extension of the Faculty of Medicine,
+                  Paa-Saviya strives to make a meaningful impact on wound care
+                  and patient outcomes.
+                </p>
+              </div>
+            </motion.div>
+          </div>
+        </div>
+      </div>
+
+      {/* Our vision section */}
+      <div className="container-fluid py-5 bg-gray my-5">
         <div className="row justify-content-center my-4">
           <motion.div
-            ref={firstSectionRef}
+            ref={secondSectionRef}
             initial={{ opacity: 0, x: -300 }}
-            animate={firstSectionInView ? { opacity: 1, x: 0 } : {}}
+            animate={secondSectionInView ? { opacity: 1, x: 0 } : {}}
             transition={{ duration: 1.2 }}
-            className="col p-5"
+            className="col-md-5 p-5"
           >
-            <p
-              style={{
-                lineHeight: "2",
-                letterSpacing: "0.05em",
-                fontSize: "1.1em",
-              }}
-              className="text-center text-muted"
-            >
-              Our goal is to be the leader in providing evidence-based,
-              comprehensive, and patient-centered care for wound management and
-              amputation prevention in Sri Lanka. We aim to establish a center
-              of excellence dedicated to advancing holistic care, pioneering
-              research, driving innovation, and delivering exceptional education
-              in wound care and amputation prevention.
-            </p>
+            <div className="custom-card card border-0">
+              <div className="card-body">
+                <p
+                  style={{
+                    lineHeight: 2,
+                    letterSpacing: "0.05em",
+                    fontSize: "1.05em",
+                  }}
+                  className="card-title m-0 text-justify"
+                >
+                  Our goal is to be the leader in providing evidence-based,
+                  comprehensive, and patient-centered care for wound management
+                  and amputation prevention in Sri Lanka. We aim to establish a
+                  center of excellence dedicated to advancing holistic care,
+                  pioneering research, driving innovation, and delivering
+                  exceptional education in wound care and amputation prevention.
+                </p>
+              </div>
+            </div>
           </motion.div>
 
           <motion.div
-            ref={firstSectionRef}
+            ref={secondSectionRef}
             initial={{ opacity: 0, x: 300 }}
-            animate={firstSectionInView ? { opacity: 1, x: 0 } : {}}
+            animate={secondSectionInView ? { opacity: 1, x: 0 } : {}}
             transition={{ duration: 1.2 }}
-            className="col-md-7 ml-auto px-5 py-3 bg-primary text-white shadow-lg d-flex align-items-center"
+            className="col-md-7 border ml-auto px-5 py-3 text-white shadow d-flex align-items-center bg-image-vision"
           >
-            <div className="ml-4">
-              <h1 className="font-weight-bold mb-4">
+            <div className="bg-overlay"></div>
+            <div className="ml-4" style={{ zIndex: 2 }}>
+              <h1 className="font-weight-bold mb-4 vision-text">
                 <div style={{ display: "flex", flexDirection: "column" }}>
-                  <span style={{ fontSize: "1.5em" }}>Our</span>
-                  <span style={{ fontSize: "2.5em" }}>Vision</span>
+                  <span className="main-title" style={{ fontSize: "1.5em" }}>
+                    Our
+                  </span>
+                  <span className="sub-title" style={{ fontSize: "2.5em" }}>
+                    Vision
+                  </span>
                 </div>
               </h1>
-            </div>
-            <div className="ml-5 px-5">
-              <BsEye style={{ fontSize: "6em", marginBottom: "0.5em" }} />
             </div>
           </motion.div>
         </div>
       </div>
 
-      <div className="container-fluid py-5 bg-gray my-5">
-        <motion.div
-          className="row justify-content-center align-items-center my-5"
-          variants={container}
-          initial="hidden"
-          animate={secondSectionInView ? "visible" : "hidden"}
-          ref={secondSectionRef}
-        >
-          <motion.div className="col-md-3 mb-4 ml-5" variants={item}>
-            <div className="card shadow-lg border-0">
-              <div className="card-body">
-                <p className="card-title">
-                  Develop a clinical service provision center for wound healing
-                  and limb salvage.
-                </p>
-              </div>
-            </div>
-          </motion.div>
-          <motion.div className="col-md-3 mb-4 mx-5" variants={item}>
-            <div className="card border-0 bg-gray">
-              <div className="card-body d-flex flex-column align-items-center">
-                <h1 className="card-title my-auto">Aims</h1>
-              </div>
-            </div>
-          </motion.div>
-          <motion.div className="col-md-3 mb-4 mr-5" variants={item}>
-            <div className="card shadow-lg border-0">
-              <div className="card-body">
-                <p className="card-title">
-                  Provide clinical training and enhance knowledge among health
-                  professionals.
-                </p>
-              </div>
-            </div>
-          </motion.div>
-          <div className="row justify-content-center mt-5">
-            <motion.div className="col-md-3 mb-4 mx-auto" variants={item}>
-              <div className="card shadow-lg border-0">
-                <div className="card-body">
-                  <p className="card-title">
-                    Conduct research and innovation with local and international
-                    collaboration.
-                  </p>
-                </div>
-              </div>
-            </motion.div>
-          </div>
-        </motion.div>
-      </div>
-
-      <div className="container-fluid py-5 bg-gray">
-        <div className="row justify-content-center my-5">
+      {/* Our Aims section */}
+      <div className="container-fluid my-5 py-2 bg-gray">
+        <div className="row justify-content-center my-5 h-100">
           <motion.div
+            style={{ minHeight: "55vh" }}
             ref={thirdSectionRef}
             initial={{ opacity: 0, x: -300 }}
             animate={thirdSectionInView ? { opacity: 1, x: 0 } : {}}
             transition={{ duration: 1.2 }}
-            className="col-md-6 ml-auto px-5 py-3 bg-primary text-white shadow-lg d-flex align-items-center justify-content-end"
+            className="col-md-6 ml-auto py-3 b text-white shadow d-flex align-items-center justify-content-end bg-image-aims"
           >
-            <div className="ml-5 px-5">
-              <FaHandsHelping
-                style={{ fontSize: "7em", marginBottom: "0.5em" }}
-              />
-            </div>
+            <div className="bg-overlay" style={{ zIndex: 2 }}></div>
+
             <div className="ml-4">
-              <h1 className="font-weight-bold mb-4">
+              <h1 className="mb-4 vision-text">
                 <div style={{ display: "flex", flexDirection: "column" }}>
-                  <span style={{ fontSize: "1.5em" }}>Our</span>
-                  <span style={{ fontSize: "2.5em" }}>Services</span>
+                  <span
+                    className="main-title font-weight-bold"
+                    style={{ fontSize: "1.5em" }}
+                  >
+                    Our
+                  </span>
+                  <span
+                    className="sub-title font-weight-bold"
+                    style={{ fontSize: "2.5em" }}
+                  >
+                    Aims
+                  </span>
                 </div>
               </h1>
             </div>
@@ -185,57 +254,41 @@ const AboutUs = () => {
             initial="hidden"
             animate={thirdSectionInView ? "visible" : "hidden"}
             variants={thirdContainer}
-            className="col-md-6 p-3"
+            className="col-md-6 py-4 px-5 d-flex flex-column justify-content-between"
             ref={thirdSectionRef}
           >
             <motion.div
               variants={thirdContainerItem}
-              className="card shadow-lg border-0 mb-2"
+              className="custom-card card border-0 m-0"
             >
-              <div className="card-body">
-                <p className="card-title m-0">
-                  Conducting education and clinical training to undergraduate,
-                  postgraduate students and healthcare professionals.
-                </p>
-              </div>
-            </motion.div>
-            <motion.div
-              variants={thirdContainerItem}
-              className="card shadow-lg border-0 mb-2"
-            >
-              <div className="card-body">
-                <p className="card-title m-0">
-                  Offer comprehensive research training and engage in
-                  collaborative research with members, institutes, and
-                  international universities.
+              <div className="card-body m-3">
+                <p className="card-title m-0 text-justify">
+                  Develop a clinical service provision center for wound healing
+                  and limb salvage.
                 </p>
               </div>
             </motion.div>
 
             <motion.div
               variants={thirdContainerItem}
-              className="card shadow-lg border-0 mb-2"
+              className="custom-card card border-0"
             >
               <div className="card-body">
-                <p className="card-title m-0">
-                  Providing clinical services in relation to wound care,
-                  podiatry, revascularization, infection diagnostics and
-                  therapeutics, foot wear and foot care advice, nutritional
-                  wellbeing, mental health and public health care.
+                <p className="card-title m-0 text-justify">
+                  Provide clinical training and enhance knowledge among health
+                  professionals.
                 </p>
               </div>
             </motion.div>
             <motion.div
               variants={thirdContainerItem}
-              className="card shadow-lg border-0 mb-2"
+              className="custom-card card border-0"
             >
               <div className="card-body">
-                <p className="card-title m-0">Diagnostic services</p>
-                <ul>
-                  <li>Non invasive and invasive perfusion assessment</li>
-                  <li>Infection diagnostics</li>
-                  <li>Biomechanical assessment of the foot</li>
-                </ul>
+                <p className="card-title m-0 text-justify">
+                  Conduct research and innovation with local and international
+                  collaboration
+                </p>
               </div>
             </motion.div>
           </motion.div>
